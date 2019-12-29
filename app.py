592c73c9c69940caf99c1b7447ff2e23a3e074c3
Radon Rosborough <radon.neon@gmail.com>
2019-12-29 15:15:25 -0700

    Add basic Flask server

---
+++ app.py
@@ -0,0 +1,23 @@
+import flask
+import json
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def get_index():
+    return flask.send_file("static/index.html")
+
+
+@app.route("/<path>")
+def get_static_file(path):
+    return flask.send_from_directory("static", path)
+
+
+@app.route("/api/v1/responses")
+def get_responses():
+    try:
+        with open("responses.json") as f:
+            return flask.jsonify(json.load(f))
+    except (OSError, json.JSONDecodeError):
+        return "Data not available", 500
