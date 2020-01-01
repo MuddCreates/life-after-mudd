0ad4926d845dff7c62ab345bc619550ef87ccb1d
Radon Rosborough <radon.neon@gmail.com>
2019-12-31 17:25:28 -0700

    Messing around with map overlays

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,32 @@
-console.log("Hello");
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicmF4b2Q1MDIiLCJhIjoiY2s0cmZmYW5kMnNodjNrbnZ0Y3E4emJxbiJ9.ejylhHRJ_dhT3uF3nQSJOA";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v9"
+});
+
+map.on("load", function() {
+  map.addLayer({
+    id: "points",
+    type: "symbol",
+    source: {
+      type: "geojson",
+      data: {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [0, 0]
+            }
+          }
+        ]
+      }
+    },
+    layout: {
+      "icon-image": "circle-15"
+    }
+  });
+});
