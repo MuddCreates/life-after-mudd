f08801176c2e3626e7ca81857f6eb380ff395a9f
Radon Rosborough <radon.neon@gmail.com>
2020-01-06 19:30:26 -0700

    Basic React/Redux app

--- static/js/index.js
+++ static/js/index.js
@@ -1,36 +1,37 @@
 // https://github.com/parcel-bundler/parcel/issues/871#issuecomment-367899522
 import "babel-polyfill";
 
+import React from "react";
+import { render } from "react-dom";
+import { hot } from "react-hot-loader";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
+
 import { initMapbox } from "./shared.js";
 
-initMapbox();
+const defaultState = {};
 
-const map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v9",
-});
+const reducers = (state = defaultState, action) => state;
 
-map.on("load", function() {
-  map.addLayer({
-    id: "points",
-    type: "symbol",
-    source: {
-      type: "geojson",
-      data: {
-        type: "FeatureCollection",
-        features: [
-          {
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [0, 0],
-            },
-          },
-        ],
-      },
-    },
-    layout: {
-      "icon-image": "circle-15",
-    },
-  });
-});
+const store = createStore(reducers);
+
+class App extends React.Component {
+  render() {
+    return "Hello";
+  }
+}
+
+function main() {
+  initMapbox();
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("mount"),
+  );
+}
+
+main();
+
+// https://dev.to/cronokirby/react-typescript-parcel-setting-up-hot-module-reloading-4f3f#setting-up-hmr
+export default hot(module)(App);
