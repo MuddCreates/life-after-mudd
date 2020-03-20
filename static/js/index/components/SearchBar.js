aff0ee932b184b583232edff8de800a4182b289f
Radon Rosborough <radon.neon@gmail.com>
2020-03-20 14:10:14 -0600

    Implement search autosuggestions

--- static/js/index/components/SearchBar.js
+++ static/js/index/components/SearchBar.js
@@ -1,10 +1,88 @@
 "use strict";
 
+import latinize from "latinize";
 import React from "react";
 import { connect } from "react-redux";
+import { UsaStates } from "usa-states";
+
+// https://github.com/parcel-bundler/parcel/issues/333#issuecomment-549273429
+//
+// Sigh. Refer to the following commit message.
+// https://bitbucket.org/sjl/dotfiles/commits/b211864a20cef443ba20212b67e3392968a27b46
+window.$ = window.jQuery = require("jquery");
+require("bootstrap");
+require("bootstrap-autocomplete");
+
+const stateMap = {};
+for (const state of new UsaStates().states) {
+  stateMap[state.abbreviation] = state;
+}
+
+function getSearchSuggestions(responses) {
+  const cities = [].concat
+    .apply(
+      [],
+      responses.map(resp => [resp.city, resp.summerCity]),
+    )
+    .sort();
+  const states = [].concat
+    .apply(
+      [],
+      responses.map(resp => [
+        stateMap[resp.state] ? stateMap[resp.state].name : "",
+        stateMap[resp.summerState] ? stateMap[resp.summerState].name : "",
+      ]),
+    )
+    .sort();
+  const countries = [].concat
+    .apply(
+      [],
+      responses.map(resp => [resp.country, resp.summerCountry]),
+    )
+    .sort();
+  const orgs = [].concat
+    .apply(
+      [],
+      responses.map(resp => [resp.org, resp.summerOrg]),
+    )
+    .sort();
+  const names = responses.map(resp => resp.name).sort();
+  const queries = new Set(
+    [].concat(
+      countries,
+      states,
+      ["Bay Area"],
+      cities,
+      orgs,
+      countries,
+      orgs,
+      names,
+    ),
+  );
+  queries.delete("");
+  return queries;
+}
+
+function normalize(query) {
+  return latinize(query)
+    .toLowerCase()
+    .replace(/[^a-z]/g, "");
+}
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.input = React.createRef();
+  }
   render() {
+    const input = (
+      <input
+        className="mapboxgl-ctrl-geocoder--input"
+        type="text"
+        placeholder="Search"
+        ref={this.input}
+      />
+    );
     return (
       <div
         style={{
@@ -16,22 +94,39 @@ class SearchBar extends React.Component {
       >
         <div className="mapboxgl-ctrl-geocoder mapboxgl-ctrl">
           <svg
-            class="mapboxgl-ctrl-geocoder--icon mapboxgl-ctrl-geocoder--icon-search"
+            className="mapboxgl-ctrl-geocoder--icon mapboxgl-ctrl-geocoder--icon-search"
             viewBox="0 0 18 18"
             width="18"
             height="18"
           >
             <path d="M7.4 2.5c-2.7 0-4.9 2.2-4.9 4.9s2.2 4.9 4.9 4.9c1 0 1.8-.2 2.5-.8l3.7 3.7c.2.2.4.3.8.3.7 0 1.1-.4 1.1-1.1 0-.3-.1-.5-.3-.8L11.4 10c.4-.8.8-1.6.8-2.5.1-2.8-2.1-5-4.8-5zm0 1.6c1.8 0 3.2 1.4 3.2 3.2s-1.4 3.2-3.2 3.2-3.3-1.3-3.3-3.1 1.4-3.3 3.3-3.3z"></path>
           </svg>
-          <input
-            className="mapboxgl-ctrl-geocoder--input"
-            type="text"
-            placeholder="Search"
-          />
+          {input}
         </div>
       </div>
     );
   }
+  componentDidMount() {
+    $(this.input.current).autoComplete({
+      resolver: "custom",
+      events: {
+        search: (query, callback) => {
+          const normQuery = normalize(query);
+          const results = [];
+          for (const item of this.props.suggestions) {
+            const normItem = normalize(item);
+            if (normItem.includes(normQuery)) {
+              results.push({ text: item });
+            }
+          }
+          callback(results);
+        },
+      },
+      minLength: 1,
+    });
+  }
 }
 
-export default connect(_ => ({}))(SearchBar);
+export default connect(state => ({
+  suggestions: state.responses && getSearchSuggestions(state.responses),
+}))(SearchBar);
