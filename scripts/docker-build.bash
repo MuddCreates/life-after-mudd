7e4360aac9770588847d8301d2bdb981e6771280
Owen Gillespie <owenlgillespie@gmail.com>
2020-03-25 23:47:11 -0400

    My feeble attempts at setting up docker compose correctly

--- scripts/docker-build.bash
+++ scripts/docker-build.bash
@@ -8,6 +8,7 @@ run=yes
 docker_build_args=()
 docker_run_opts=()
 docker_run_args=()
+export DOCKERFILE="Dockerfile"
 
 if [[ "$1" == "--prod" || "$1" == "--prod-run" ]]; then
     tag=latest
@@ -22,33 +23,20 @@ if [[ "$1" == "--prod" || "$1" == "--prod-run" ]]; then
         run=
     fi
 else
-    docker_build_args=(
-        "${docker_build_args[@]}"
-        -f Dockerfile.dev
-        --build-arg "UID=$UID"
-    )
-    docker_run_opts=(
-        "${docker_run_opts[@]}"
-        -v "$PWD:/home/docker/src"
-    )
+    export DOCKERFILE="Dockerfile.dev"
+    export REDIS_URL="redis://localhost:6379"
+    # export VOLUME_PATH="/home/docker/src"
+    # I'm not sure what this is meant to do, but I'm sure that I'm breaking it
     if [[ -n "$1" ]]; then
         docker_run_args=(bash -c "$1")
     fi
 fi
 
-scripts/docker.bash                      \
-    build . -t "life-after-mudd:${tag}"  \
-    "${docker_build_args[@]}"
+scripts/docker-compose.bash build
 
 PORT="${PORT:-8080}"
 HMR_PORT="${HMR_PORT:-8081}"
 HOST="${HOST:-127.0.0.1}"
 if [[ -n "$run" ]]; then
-    scripts/docker.bash                                                    \
-        run -it --rm                                                       \
-        -e PORT -e HMR_PORT                                                \
-        -p "${HOST}:${PORT}:${PORT}" -p "${HOST}:${HMR_PORT}:${HMR_PORT}"  \
-        -e LAM_OAUTH_PRIVATE_KEY                                           \
-        "${docker_run_opts[@]}"                                            \
-        "life-after-mudd:${tag}" "${docker_run_args[@]}"
+    scripts/docker-compose.bash run
 fi
