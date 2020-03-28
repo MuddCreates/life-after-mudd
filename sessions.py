3ca495c2d1551aa4355c499209e57b85334404b5
Owen Gillespie <owenlgillespie@gmail.com>
2020-03-28 15:29:13 -0400

    Handle failure to connect to redis, and extend prefix

--- sessions.py
+++ sessions.py
@@ -2,15 +2,25 @@ import os
 import redis
 
 # Some characters at the end of the token change each time, so we only use the
-# first 40 characters to identify users
-PREFIX_LENGTH = 40
+# The first time dependent field is ~500 characters in (at_hash)
+PREFIX_LENGTH = 475
+SET_NAME = "TOKENS"
 
 # Heroku sets REDIS_URL to access the redis server
-r = redis.from_url(os.environ.get("REDIS_URL"))
-
+redis_url = os.environ.get("REDIS_URL")
+r = None
+if redis_url is not None:
+    try:
+        r = redis.from_url(redis_url)
+    except:
+        print(f"failed to connect to redis at url: {redis_url}")
 
 def add_token(token, expiration_seconds=500):
-    r.setex(token[:PREFIX_LENGTH], expiration_seconds, "1")
+    if r is not None:
+        r.sadd(SET_NAME, token[:PREFIX_LENGTH])
 
 def check_token(token):
-    return r.exists(token[:PREFIX_LENGTH])
+    if r is not None:
+        return r.sismember(SET_NAME, token[:PREFIX_LENGTH])
+    else:
+        return False
