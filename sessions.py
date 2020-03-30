bae4af419ff37718645b044620d6f712b3c7be22
Radon Rosborough <radon.neon@gmail.com>
2020-03-29 19:27:50 -0600

    Improve comments on module constants

--- sessions.py
+++ sessions.py
@@ -8,9 +8,17 @@ import redis
 # differs for each issuance. We want to use OAuth tokens as a way to
 # identify when we are seeing the same user more than once, so we
 # employ the following disgusting hack to just restrict our attention
-# to the part of the token that doesn't change.
+# to the part of the token that doesn't change. This variable holds
+# the length of the prefix that we consider.
 PREFIX_LENGTH = 475
+
+# Redis key used to store the set of known-good tokens.
 SET_NAME = "TOKENS"
+
+# How many seconds it takes before we forget about OAuth tokens we
+# have seen and we'll re-do the API call to validate them if we see
+# them again.
+OAUTH_CACHE_TIMEOUT = 30 * 86400  # one month
 
 # In production, we expect REDIS_URL to be set by Heroku. In
 # development, it comes from docker-compose.yml.
@@ -26,7 +34,7 @@ if redis_url is not None:
         )
 
 
-def add_token(token, expiration_seconds=500):
+def add_token(token, expiration_seconds=OAUTH_CACHE_TIMEOUT):
     if r is not None:
         r.sadd(SET_NAME, token[:PREFIX_LENGTH])
 
