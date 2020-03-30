35a5926358491fc88aa76b42d32d8321ef000621
Radon Rosborough <radon.neon@gmail.com>
2020-03-29 19:28:50 -0600

    Minor cleanup

--- sessions.py
+++ sessions.py
@@ -23,8 +23,9 @@ OAUTH_CACHE_TIMEOUT = 30 * 86400  # one month
 # In production, we expect REDIS_URL to be set by Heroku. In
 # development, it comes from docker-compose.yml.
 redis_url = os.environ.get("REDIS_URL")
+
 r = None
-if redis_url is not None:
+if redis_url:
     try:
         r = redis.from_url(redis_url)
     except Exception:
@@ -32,15 +33,17 @@ if redis_url is not None:
             f"Failed to connect to Redis, disabling OAuth caching: {redis_url}",
             file=sys.stderr,
         )
+else:
+    print("Redis URL unset, disabling OAuth caching", file=sys.stderr)
 
 
 def add_token(token, expiration_seconds=OAUTH_CACHE_TIMEOUT):
-    if r is not None:
+    if r:
         r.sadd(SET_NAME, token[:PREFIX_LENGTH])
 
 
 def check_token(token):
-    if r is not None:
+    if r:
         return r.sismember(SET_NAME, token[:PREFIX_LENGTH])
     else:
         return False
