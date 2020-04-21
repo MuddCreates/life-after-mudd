2d0bad0f35cd32b5835201930efec9886d985c0d
Radon Rosborough <radon.neon@gmail.com>
2020-04-20 21:02:16 -0600

    Jesus I have no words for how terrible this was

--- static/js/index/components/MapView.js
+++ static/js/index/components/MapView.js
@@ -13,10 +13,15 @@ import {
 } from "../config";
 import { tagAll } from "../tag";
 import { store } from "../redux";
-import { allowResizingWindow, originalWindowHeight } from "../util";
+import {
+  allowResizingWindow,
+  originalWindowHeight,
+  originalWindowWidth,
+} from "../util";
 
 const CIRCLE_RADIUS = 10;
 const HIGHLIGHT_BBOX_RADIUS = 10;
+const POINT_PADDING = searchBarOcclusion + 30;
 
 const Map = ReactMapboxGl({
   accessToken: mapboxAccessToken,
@@ -32,7 +37,7 @@ class MapView extends React.Component {
     // actions dispatched by another component. Sad. Anyway, this
     // yucky hack allows us to re-zoom the map on each search query,
     // while otherwise not messing with it.
-    this.lastBounds = [
+    this.initialBounds = [
       [-126, 24],
       [-66, 50],
     ];
@@ -79,74 +84,21 @@ class MapView extends React.Component {
         </Layer>
       );
     }
-    let bounds = this.lastBounds;
-    let animated = false;
-    if (
-      this.lastSerial !== this.props.serial &&
-      this.props.displayedResponses.size > 0
-    ) {
-      let left = Infinity;
-      let right = -Infinity;
-      let top = -Infinity;
-      let bottom = Infinity;
-      this.props.responses
-        .filter((resp) => this.props.displayedResponses.has(resp.idx))
-        .forEach((resp) => {
-          left = Math.min(left, resp.tag.latLong.lng);
-          right = Math.max(right, resp.tag.latLong.lng);
-          bottom = Math.min(bottom, resp.tag.latLong.lat);
-          top = Math.max(top, resp.tag.latLong.lat);
-        });
-      // There is a limit for how much we zoom in.
-      const zoomLimit = 0.2; // minimum width of window in degrees
-      let horizDisp = right - left;
-      if (horizDisp < zoomLimit) {
-        left -= (zoomLimit - horizDisp) / 2;
-        right += (zoomLimit - horizDisp) / 2;
-        horizDisp = zoomLimit;
-      }
-      let vertDisp = top - bottom;
-      if (vertDisp < zoomLimit) {
-        bottom -= (zoomLimit - vertDisp) / 2;
-        top += (zoomLimit - vertDisp) / 2;
-        vertDisp = zoomLimit;
-      }
-      // Add extra margin just for show.
-      left -= horizDisp / 3;
-      right += horizDisp / 3;
-      bottom -= vertDisp / 3;
-      top += vertDisp / 3;
-      // Account for sidebar and search bar.
-      const searchBarHeightFraction = searchBarOcclusion / window.innerHeight;
-      const prevWidth = right - left;
-      const prevHeight = top - bottom;
-      if (this.props.sidebarVertical) {
-        right +=
-          (prevWidth * sidebarWidthFraction) / (1 - sidebarWidthFraction);
-      } else {
-        bottom -=
-          (prevHeight * sidebarHeightFraction) / (1 - sidebarHeightFraction);
-      }
-      top +=
-        (prevHeight * searchBarHeightFraction) / (1 - searchBarHeightFraction);
-      bounds = [
-        [left, bottom],
-        [right, top],
-      ];
-      this.lastSerial = this.props.serial;
-      this.lastBounds = bounds;
-      animated = true;
-    }
     return (
       <div id="mapContainerContainer">
         <Map
           style="mapbox://styles/raxod502/ck6nxepcj03jv1jqe6a7p8om4"
-          fitBounds={bounds}
-          fitBoundsOptions={{ duration: animated ? 3000 : 0 }}
+          fitBounds={this.initialBounds}
+          fitBoundsOptions={{ duration: 0 }}
           onClick={this.onMouseEvent}
           onMouseDown={this.onMouseEvent}
           onMouseMove={this.onMouseEvent}
           onMouseUp={this.onMouseEvent}
+          onStyleLoad={(map) => {
+            // Need to extract the actual Mapbox map so we can use it
+            // from componentDidUpdate.
+            this.map = map;
+          }}
           containerStyle={{
             height: allowResizingWindow()
               ? "100vh"
@@ -156,6 +108,110 @@ class MapView extends React.Component {
           {layer}
         </Map>
       </div>
+    );
+  }
+  componentDidUpdate() {
+    if (!this.map) {
+      // Shouldn't happen but who knows :/
+      return;
+    }
+    if (this.props.serial === this.lastSerial) {
+      return;
+    }
+    this.lastSerial = this.props.serial;
+    if (this.props.displayedResponses.size === 0) {
+      return;
+    }
+    // OK. This long sequence of geometry code is basically
+    // impossible to read. I know and am sorry. I'm not happy about
+    // it either.
+    let mapLeft = Infinity;
+    let mapRight = -Infinity;
+    let mapTop = -Infinity;
+    let mapBottom = Infinity;
+    this.props.responses
+      .filter((resp) => this.props.displayedResponses.has(resp.idx))
+      .forEach((resp) => {
+        mapLeft = Math.min(mapLeft, resp.tag.latLong.lng);
+        mapRight = Math.max(mapRight, resp.tag.latLong.lng);
+        mapBottom = Math.min(mapBottom, resp.tag.latLong.lat);
+        mapTop = Math.max(mapTop, resp.tag.latLong.lat);
+      });
+    const zoomLimit = 0.2;
+    if (mapRight - mapLeft < zoomLimit) {
+      const offset = (zoomLimit - (mapRight - mapLeft)) / 2;
+      mapLeft -= offset;
+      mapRight += offset;
+    }
+    if (mapTop - mapBottom < zoomLimit) {
+      const offset = (zoomLimit - (mapTop - mapBottom)) / 2;
+      mapBottom -= offset;
+      mapTop += offset;
+    }
+    let { x: screenLeft, y: screenBottom } = this.map.project([
+      mapLeft,
+      mapBottom,
+    ]);
+    let { x: screenRight, y: screenTop } = this.map.project([mapRight, mapTop]);
+    const screenWidth = screenRight - screenLeft;
+    const screenHeight = screenBottom - screenTop;
+    let viewportLeft = 0;
+    let viewportRight = originalWindowWidth;
+    let viewportTop = 0;
+    let viewportBottom = originalWindowHeight;
+    if (this.props.sidebarVertical) {
+      viewportRight -= sidebarWidthFraction * originalWindowWidth;
+    } else {
+      viewportBottom -= sidebarHeightFraction * originalWindowHeight;
+    }
+    viewportLeft += POINT_PADDING;
+    viewportRight -= POINT_PADDING;
+    viewportTop += POINT_PADDING;
+    viewportBottom -= POINT_PADDING;
+    const viewportWidth = viewportRight - viewportLeft;
+    const viewportHeight = viewportBottom - viewportTop;
+    const scale = Math.min(
+      viewportWidth / screenWidth,
+      viewportHeight / screenHeight,
+    );
+    const scaledWidth = scale * screenWidth;
+    const scaledHeight = scale * screenHeight;
+    const scaledWidthPadding = (viewportWidth - scaledWidth) / 2;
+    const scaledHeightPadding = (viewportHeight - scaledHeight) / 2;
+    const scaledLeft = viewportLeft + scaledWidthPadding;
+    const scaledRight = viewportRight - scaledWidthPadding;
+    const scaledBottom = viewportBottom - scaledHeightPadding;
+    const scaledTop = viewportTop + scaledHeightPadding;
+    const revisedScreenLeft =
+      screenLeft -
+      ((screenRight - screenLeft) / (scaledRight - scaledLeft)) *
+        (scaledLeft - 0);
+    const revisedScreenRight =
+      screenRight +
+      ((screenRight - screenLeft) / (scaledRight - scaledLeft)) *
+        (originalWindowWidth - scaledRight);
+    const revisedScreenTop =
+      screenTop -
+      ((screenBottom - screenTop) / (scaledBottom - scaledTop)) *
+        (scaledTop - 0);
+    const revisedScreenBottom =
+      screenBottom +
+      ((screenBottom - screenTop) / (scaledBottom - scaledTop)) *
+        (originalWindowHeight - scaledBottom);
+    ({ lng: mapLeft, lat: mapBottom } = this.map.unproject([
+      revisedScreenLeft,
+      revisedScreenBottom,
+    ]));
+    ({ lng: mapRight, lat: mapTop } = this.map.unproject([
+      revisedScreenRight,
+      revisedScreenTop,
+    ]));
+    this.map.fitBounds(
+      [
+        [mapLeft, mapBottom],
+        [mapRight, mapTop],
+      ],
+      { duration: 3000 },
     );
   }
   getNearbyPoints = (map, point) => {
@@ -173,6 +229,7 @@ class MapView extends React.Component {
     return nearbyPoints;
   };
   onMouseEvent = (map, e) => {
+    window.map = map;
     if (!map.getLayer("people")) {
       // Map not fully loaded yet, refrain from messing with it to
       // avoid errors.
