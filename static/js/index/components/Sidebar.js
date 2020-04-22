971e5ed15f615ef453bd683410e9b9d89ba79767
Radon Rosborough <radon.neon@gmail.com>
2020-04-22 08:32:25 -0600

    Run Prettier

--- static/js/index/components/Sidebar.js
+++ static/js/index/components/Sidebar.js
@@ -14,7 +14,11 @@ import {
   originalWindowHeight,
 } from "../util";
 
-function groupPlansConfigurable(responses, getFirstGroupKey, getSecondGroupKey) {
+function groupPlansConfigurable(
+  responses,
+  getFirstGroupKey,
+  getSecondGroupKey,
+) {
   const index = {};
   for (const resp of responses) {
     const firstGroupKey = getFirstGroupKey(resp);
@@ -34,18 +38,31 @@ function groupPlansConfigurable(responses, getFirstGroupKey, getSecondGroupKey) 
       firstKey,
       secondKeys: Object.keys(index[firstKey])
         .sort()
-        .map((secondKey) => ({ secondKey, responses: index[firstKey][secondKey] })),
+        .map((secondKey) => ({
+          secondKey,
+          responses: index[firstKey][secondKey],
+        })),
     }));
 }
 
-function createTwoLevelViewJSX(taggedData, firstKeyView, secondKeyView, firstGroupBy, secondGroupBy){
+function createTwoLevelViewJSX(
+  taggedData,
+  firstKeyView,
+  secondKeyView,
+  firstGroupBy,
+  secondGroupBy,
+) {
   const groupedData = groupPlansConfigurable(
-    taggedData, firstGroupBy, secondGroupBy,
+    taggedData,
+    firstGroupBy,
+    secondGroupBy,
   );
   const firstKeyAction = (secondKeys) => {
     store.dispatch({
       type: "SHOW_DETAILS",
-      responses: secondKeys.flatMap(item => item.responses.map(resp => resp.idx)),
+      responses: secondKeys.flatMap((item) =>
+        item.responses.map((resp) => resp.idx),
+      ),
       sidebarView: firstKeyView,
     });
     store.dispatch({
@@ -56,57 +73,62 @@ function createTwoLevelViewJSX(taggedData, firstKeyView, secondKeyView, firstGro
   const secondKeyAction = (responses) => {
     store.dispatch({
       type: "SHOW_DETAILS",
-      responses: responses.map(resp => resp.idx),
+      responses: responses.map((resp) => resp.idx),
       sidebarView: secondKeyView,
     });
     store.dispatch({
       type: "UPDATE_MAP_VIEW_ZOOM",
     });
   };
-  return (
-          groupedData.map(({ firstKey, secondKeys}, idx) => (
-            <Fragment key={idx}>
-              <h5 onClick={() => firstKeyAction(secondKeys)}>
-                <b>{firstKey}</b>
-              </h5>
-              {secondKeys.map(({ secondKey, responses }, idx) => (
-                <Fragment key={idx}>
-                  <b onClick={()=>secondKeyAction(responses)}>{secondKey}</b>
-                  {responses.map((resp, idx) => (
-                      <div
-                    key={idx}
-                    onClick={
-                      () => {
-                        store.dispatch({
-                        type: "SHOW_DETAILS",
-                        responses: [resp.idx],
-                          sidebarView: SidebarView.detailView,
-                        });
-                      }
-                    }>{resp.name || "Anonymous"}</div>
-                  ))}
-                </Fragment>
-              ))}
-            </Fragment>
-          ))
-  );
+  return groupedData.map(({ firstKey, secondKeys }, idx) => (
+    <Fragment key={idx}>
+      <h5 onClick={() => firstKeyAction(secondKeys)}>
+        <b>{firstKey}</b>
+      </h5>
+      {secondKeys.map(({ secondKey, responses }, idx) => (
+        <Fragment key={idx}>
+          <b onClick={() => secondKeyAction(responses)}>{secondKey}</b>
+          {responses.map((resp, idx) => (
+            <div
+              key={idx}
+              onClick={() => {
+                store.dispatch({
+                  type: "SHOW_DETAILS",
+                  responses: [resp.idx],
+                  sidebarView: SidebarView.detailView,
+                });
+              }}
+            >
+              {resp.name || "Anonymous"}
+            </div>
+          ))}
+        </Fragment>
+      ))}
+    </Fragment>
+  ));
 }
 
-function describeSubject(taggedSubject){
+function describeSubject(taggedSubject) {
   let fields = [];
-  if(taggedSubject.tag.desc && taggedSubject.tag.loc){
-    fields.push(<div>{taggedSubject.tag.desc} in {taggedSubject.tag.loc}</div>);
+  if (taggedSubject.tag.desc && taggedSubject.tag.loc) {
+    fields.push(
+      <div>
+        {taggedSubject.tag.desc} in {taggedSubject.tag.loc}
+      </div>,
+    );
   }
   if (taggedSubject.major) {
     fields.push(<div>Majored in {taggedSubject.major}</div>);
   }
-  if (taggedSubject.comments){
+  if (taggedSubject.comments) {
     fields.push(<div>Note: {taggedSubject.comments}</div>);
   }
   // TODO: Figure out how to cleanly add keys to fields elements
   return (
-      <div>
-      <h5><b> {taggedSubject.name}</b></h5>
+    <div>
+      <h5>
+        <b> {taggedSubject.name}</b>
+      </h5>
       {fields}
     </div>
   );
@@ -117,7 +139,9 @@ class Sidebar extends React.Component {
     let sidebarContent = null;
     const taggedData = tagAll(this.props.responses, this.props.geotagView);
     let groupedData = groupPlansConfigurable(
-      tagAll(this.props.responses, this.props.geotagView), resp => resp.tag.loc, resp => resp.tag.desc,
+      tagAll(this.props.responses, this.props.geotagView),
+      (resp) => resp.tag.loc,
+      (resp) => resp.tag.desc,
     );
     const style = {
       position: "absolute",
@@ -151,28 +175,35 @@ class Sidebar extends React.Component {
     }
 
     let sidebarBody = null;
-    if (this.props.sidebarView === SidebarView.detailView){
+    if (this.props.sidebarView === SidebarView.detailView) {
       const subject = tag(this.props.responses[0], this.props.geotagView);
       sidebarBody = describeSubject(subject);
     } else {
       // for all view made with createTwoLevelViewJSX
-      switch (this.props.sidebarView){
-      case SidebarView.summaryView:
-        sidebarBody = createTwoLevelViewJSX(taggedData, SidebarView.summaryView, SidebarView.organizationView, resp => resp.tag.loc, resp => resp.tag.desc);
-        break;
+      switch (this.props.sidebarView) {
+        case SidebarView.summaryView:
+          sidebarBody = createTwoLevelViewJSX(
+            taggedData,
+            SidebarView.summaryView,
+            SidebarView.organizationView,
+            (resp) => resp.tag.loc,
+            (resp) => resp.tag.desc,
+          );
+          break;
 
-      case SidebarView.organizationView:
-        sidebarBody = createTwoLevelViewJSX(taggedData, SidebarView.organizationView, SidebarView.summaryView, resp => resp.tag.desc, resp => resp.tag.loc);
-        break;
+        case SidebarView.organizationView:
+          sidebarBody = createTwoLevelViewJSX(
+            taggedData,
+            SidebarView.organizationView,
+            SidebarView.summaryView,
+            (resp) => resp.tag.desc,
+            (resp) => resp.tag.loc,
+          );
+          break;
       }
     }
-    return (
-        <div style={style}>
-        {sidebarBody}
-        </div>
-    );
-  };
-
+    return <div style={style}>{sidebarBody}</div>;
+  }
 }
 
 export default connect((state) => {
